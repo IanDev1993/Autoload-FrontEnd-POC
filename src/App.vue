f851205350e7dc0f09364ae60778cc02c2c74780
Ian Dev <ian.dev.1993@gmail.com>
2024-04-06 22:49:02 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import DataTable from './components/DataTable.vue';
+
+import {ref, onMounted} from "vue";
+
+const items = ref([]);
+
+onMounted( async () => {
+   const response = await fetch('http://localhost:5008/order');
+   items.value = await response.json(); 
+});
+</script>
+
+
+<template>
+ <div class="p-8 bg-gray-300 min-h-screen">
+    <DataTable :items="items"/>
+ </div>
+</template>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    items: {
+        type: Array,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <table>
+        <tr v-for="item in items" :key="item.id">        
+            <td>{{ item.code }}</td>
+            <td>{{ item.createdAt }}</td>
+            <td>{{ item.status }}</td>
+        </tr>
+    </table>
+</template>
