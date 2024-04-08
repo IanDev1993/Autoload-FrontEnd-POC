0b338f55f338951cfe463269033a0ddc3e4c0a61
Ian Dev <ian.dev.1993@gmail.com>
2024-04-08 00:50:05 -0300

    feat: add table template css

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -8,11 +8,31 @@ defineProps({
 </script>
 
 <template>
-    <table>
-        <tr v-for="item in items" :key="item.id">        
-            <td>{{ item.code }}</td>
-            <td>{{ item.createdAt }}</td>
-            <td>{{ item.status }}</td>
-        </tr>
-    </table>
+    <div class="bg-white relative border rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr>
+                    <th class="px-4 py-3">Code</th>
+                    <th class="px-4 py-3">CreateAt</th>
+                    <th class="px-4 py-3">Status</th>
+                    <th class="px-4 py-3">ID</th>
+                    <th class="px-4 py-3">
+                        <span class="sr-only">Actions</span>
+                    </th>
+                </tr>
+    
+            </thead>
+            <tbody>
+                <tr v-for="item in items" :key="item.id" class="border-b">        
+                    <td class="px-4 py-3 font-medium text-gray-900">{{ item.code }}</td>
+                    <td class="px-4 py-3">{{ item.createdAt }}</td>
+                    <td class="px-4 py-3">{{ item.status }}</td>
+                    <td class="px-4 py-3 font-medium text-gray-900">{{ item.id }}</td>
+                    <td class="px-4 py-3 flex items-center justify-end">
+                     <a href="#" class="text-indigo-500 hover:underline">Details</a>
+                    </td>
+                </tr>                    
+            </tbody>
+        </table>
+    </div>
 </template>
