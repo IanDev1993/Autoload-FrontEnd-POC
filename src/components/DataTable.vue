33ab5475f5e6e16a5c47122838f3de3d0581bac0
Ian Dev <ian.dev.1993@gmail.com>
2024-04-27 15:03:41 -0300

    refactor: adjust clean code

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,52 +1,51 @@
+
 <script setup>
-import { ref, computed } from 'vue';
-import SearchForm from './SearchForm.vue';
-import FilterDropDown from './FilterDropDown.vue';
-import FilterRadios from './FilterRadios.vue';
 
-const searchFilter = ref('');
-const radioFilter = ref('');
+    import { ref, computed } from 'vue';
+    import SearchForm from './SearchForm.vue';
+    import FilterDropDown from './FilterDropDown.vue';
+    import FilterRadios from './FilterRadios.vue';
 
-const props = defineProps({
+    const searchFilter = ref('');
+    const radioFilter = ref('');
+
+    const props = defineProps({
+        
+        items: {
+            type: Array,
+            required: true
+        }
+
+    });
+
+    const filterdItems = computed(()=>{
+        
+        let items = props.items;
+        
+        switch(radioFilter.value){
+            case 'today':
+                items = items.filter(item=> new Date(item.createdAt).getDate() == new Date().getDate());
+                break;  
+        };
+
+        if(searchFilter.value != '')
+            items = items.filter(item => item.code.includes(searchFilter.value))  
     
-    items: {
-        type: Array,
-        required: true
-    }
+        return items;
 
-});
+    });
 
-const filterdItems = computed(()=>{
-    
-    let items = props.items;
-    
-    switch(radioFilter.value){
-        case 'today':
-            items = items.filter(item=> new Date(item.createdAt).getDate() == new Date().getDate());
-            break;
-        case 'past':
-        items = items.filter(item=> new Date(item.createdAt).getDate() < new Date());
-            break;
+    const handleSearch = (search)=>{
+
+        searchFilter.value = search;
+
     };
 
-    if(searchFilter.value != '')
-        items = items.filter(item => item.code.includes(searchFilter.value))  
-   
-    return items;
+    const handleRadioFilter = (filter)=>{
 
-});
+        radioFilter.value = filter;
 
-const handleSearch = (search)=>{
-
-    searchFilter.value = search;
-
-};
-
-const handleRadioFilter = (filter)=>{
-
-    radioFilter.value = filter;
-
-};
+    };
 
 </script>
 
@@ -62,14 +61,13 @@ const handleRadioFilter = (filter)=>{
         <table class="w-full text-xl text-left text-gray-500 font-mono">
             <thead class="text-xl text-gray-700  bg-gray-100">
                 <tr>
-                    <th class="px-4 py-3">Code</th>
+                    <th class="px-4 py-3">Order Code</th>
                     <th class="px-4 py-3">Created At</th>
                     <th class="px-4 py-3">Status</th>          
                     <th class="px-4 py-3">
                         <span class="sr-only">Actions</span>
                     </th>
-                </tr>
-    
+                </tr>    
             </thead>
             <tbody>
                 <tr v-for="item in filterdItems" :key="item.id" class="border-b">        
@@ -79,7 +77,7 @@ const handleRadioFilter = (filter)=>{
                     <td class="px-4 py-3 flex items-center justify-end">
                      <a href="#" v-show="!item.status" class="text-indigo-500 hover:underline">Validate</a>
                     </td>
-                </tr>                    
+                </tr>
             </tbody>
         </table>
     </div>
--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,19 +1,20 @@
-<template>
 
-<form action="" class="py-3 px-4 flex items-center">
-    <label for="" class="sr-only">Search</label>
-    <div class="relative w-full">
-        <input type="search" @input="search" placeholder="Search code..." class="py-1 px-2 bg-gray-100 border-gray-200 text-gray-900 text-lg caret-red-500" />
-    </div>
-</form>
-
-
-</template>
 <script setup>
-const emit = defineEmits(['search']);
-const search = (e)=>{
-   emit('search',e.target.value);
-};
 
+    const emit = defineEmits(['search']);
+
+    const search = (e)=>{
+
+     emit('search',e.target.value);
+
+    };
 
 </script>
+
+<template>
+    <form class="py-3 px-4 flex items-center">    
+        <div class="relative w-full">
+            <input type="search" @input="search" placeholder="Search code..." class="py-1 px-2 bg-gray-100 border-gray-200 text-gray-900 text-lg caret-red-500" />
+        </div>
+    </form>
+</template>
